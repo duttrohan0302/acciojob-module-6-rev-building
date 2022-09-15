833135e31ab8d2e1dbf7331e32dfd23931ebba4a
duttrohan0302 <duttrohan0302@gmail.com>
2022-09-15 22:52:49 +0530

    todos script with local storage

--- 1.15-09-22/todos/script.js
+++ 1.15-09-22/todos/script.js
@@ -2,25 +2,55 @@ const form = document.getElementById("form")
 const input = document.getElementById("input")
 const todosUL = document.getElementById("todos")
 
-const addTodo = () => {
+const initialTodos = JSON.parse(localStorage.getItem("todos"))
+
+
+
+const updateLocalStorage = () => {
+    const todoEls = document.querySelectorAll("li")
+
+    const todos = []
+
+    todoEls.forEach(todoEl=>{
+        todos.push({
+            text: todoEl.innerText,
+            isCompleted: todoEl.classList.contains("completed")
+        })
+    })
+
+    localStorage.setItem("todos",JSON.stringify(todos))
+}
+const addTodo = (todo) => {
     let todoText = input.value
+
+    if(todo){
+        todoText = todo.text
+    }
 
     if(todoText){
         const todoEl = document.createElement("li")
 
         todoEl.innerText = todoText
 
+        if(todo && todo.isCompleted){
+            todoEl.classList.add("completed")
+        }
+
         todoEl.addEventListener("click",()=>{
             todoEl.classList.toggle("completed")
+            updateLocalStorage()
         })
 
         todoEl.addEventListener("contextmenu",(e)=>{
             e.preventDefault()
 
             todoEl.remove()
+            updateLocalStorage()
         })
-
         todosUL.appendChild(todoEl)
+        input.value = ''
+        updateLocalStorage()
+
     }
 }
 
@@ -30,3 +60,6 @@ form.addEventListener("submit",(e)=>{
     addTodo()
 })
 
+if(initialTodos){
+    initialTodos.forEach(todo=>addTodo(todo))
+}
