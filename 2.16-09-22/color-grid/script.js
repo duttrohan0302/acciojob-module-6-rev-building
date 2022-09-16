38b96f3415fe31a9046ed353050b6ff0ce14f0e0
duttrohan0302 <duttrohan0302@gmail.com>
2022-09-16 22:32:05 +0530

    script for color grid

--- 2.16-09-22/color-grid/script.js
+++ 2.16-09-22/color-grid/script.js
@@ -1,5 +1,30 @@
 // declare the variables
+const reset = document.getElementById("reset_button")
+const change = document.getElementById("change_button")
+
+const blockId = document.getElementById("block_id")
+const colorId = document.getElementById("color_id")
 
 // define functions here
+const resetGrid = () => {
+    for(let i=1;i<=9;i++){
+        const gridItem = document.getElementById(i);
+        gridItem.style.backgroundColor = "transparent";
+    }
+}
+
+const colorGrid = () => {
+    if(!blockId.value && !colorId.value && blockId.value<=9 && blockId.value>=0){
+        alert("Please Enter valid block id and color id")
+    }
+
+    const gridItem = document.getElementById(blockId.value)
+    gridItem.style.backgroundColor = colorId.value
+
+    blockId.value=""
+    colorId.value=""
+}
 
 // add event listeners and call functions
+reset.addEventListener("click",resetGrid)
+change.addEventListener("click",colorGrid)
